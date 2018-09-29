989b281839b77c237dcddcd6d66623987652adb6
MoNsTeR <zgm_striker17@hotmail.com>
2018-09-29 13:59:51 -0400

    Added Mongo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,28 +1,36 @@
 const list = [];
 module.exports = function (app) {
-    app.post('/add', function (req, res) {
-        const todoObject = {
-            name: req.body.TodoItem,
-            completed: false
-        }
-        list.push(todoObject);
-        res.redirect('/');
-    })
-    app.get('/', function (req, res) {
-        res.json(list);
-    })
+
     app.get('/api/list', function (req, res) {
-        res.json(list);
-    });
-    app.delete('/delete', function (req, res) {
-        list.splice(req.body.index, 1);
-        res.json(list);
-        console.log(list);
+        db.list.find({})
+            .then(function (dbList) {
+                res.json(dbList);
+            })
+            .catch(function (err) {
+                res.json(err);
+            })
     })
-    app.put('/api/update', (req, res) => {
-        list[req.body.ID].completed = !list[req.body.ID].completed;
-        // res.json(list);
-        res.status(200).json(list);
-        // db.CollectionName.findOneAndUpdate({ _id: req.params.id }, {$set: { completed: req.body.completed}})
-    });
+};
+
+app.post('/add', function (req, res) {
+    const todoObject = {
+        name: req.body.TodoItem,
+        completed: false
+    }
+    list.push(todoObject);
+    res.redirect('/');
+});
+
+app.delete('/delete', function (req, res) {
+    list.splice(req.body.index, 1);
+    res.json(list);
+    console.log(list);
+});
+
+app.put('/api/update', (req, res) => {
+    list[req.body.ID].completed = !list[req.body.ID].completed;
+    // res.json(list);
+    res.status(200).json(list);
+    // db.CollectionName.findOneAndUpdate({ _id: req.params.id }, {$set: { completed: req.body.completed}})
+});
 }
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+mongoose.connect('mongodb://zmiles1:hfwuxyvH6VsEVEr@ds119113.mlab.com:19113/firstdb', { useNewUrlParser: true });
 require('./routes/api-routes.js')(app);
 app.listen(PORT, function () {
     console.log(`App is now listening on PORT ${PORT}`)
