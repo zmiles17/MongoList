93e2933e014b302123aa0eef527f899a04538b8d
MoNsTeR <zgm_striker17@hotmail.com>
2018-09-29 17:34:04 -0400

    Need to add Mongo Schema

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,27 +10,27 @@ module.exports = function (app) {
                 res.json(err);
             })
     })
-};
 
-app.post('/add', function (req, res) {
-    const todoObject = {
-        name: req.body.TodoItem,
-        completed: false
-    }
-    list.push(todoObject);
-    res.redirect('/');
-});
 
-app.delete('/delete', function (req, res) {
-    list.splice(req.body.index, 1);
-    res.json(list);
-    console.log(list);
-});
+    app.post('/add', function (req, res) {
+        const todoObject = {
+            name: req.body.TodoItem,
+            completed: false
+        }
+        list.push(todoObject);
+        res.redirect('/');
+    });
 
-app.put('/api/update', (req, res) => {
-    list[req.body.ID].completed = !list[req.body.ID].completed;
-    // res.json(list);
-    res.status(200).json(list);
-    // db.CollectionName.findOneAndUpdate({ _id: req.params.id }, {$set: { completed: req.body.completed}})
-});
+    app.delete('/delete', function (req, res) {
+        list.splice(req.body.index, 1);
+        res.json(list);
+        console.log(list);
+    });
+
+    app.put('/api/update', (req, res) => {
+        list[req.body.ID].completed = !list[req.body.ID].completed;
+        // res.json(list);
+        res.status(200).json(list);
+        // db.CollectionName.findOneAndUpdate({ _id: req.params.id }, {$set: { completed: req.body.completed}})
+    });
 }
